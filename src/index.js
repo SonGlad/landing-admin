5dc7fd56215583f5a2acd1f92c1548cf96256cba
SonGlad <okoshevy@gmail.com>
2024-03-28 14:56:29 +0200

    Changes for Production

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ root.render(
   <React.StrictMode>
     <Provider store = {store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename='/landing-admin'>
+        <BrowserRouter>
           <ThemeProvider theme={theme}>
             <GlobalStyle/>
             <App />
